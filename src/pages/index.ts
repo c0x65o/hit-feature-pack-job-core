94f01cc19d11a9bd039bde141eb13fe6645df4df
Clinton Mills <clinton.mills@spartancyberservices.com>
2026-01-01 15:51:31 -0600

    chore: remove AI trace features and update package versions

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,4 @@ export { TaskList, default as TaskListPage } from './TaskList';
 export { TaskDetail, default as TaskDetailPage } from './TaskDetail';
 export { TaskExecution, default as TaskExecutionPage } from './TaskExecution';
 export { AllExecutions, default as AllExecutionsPage } from './AllExecutions';
-export { AiTraces, default as AiTracesPage } from './AiTraces';
-export { AiTraceDetail, default as AiTraceDetailPage } from './AiTraceDetail';
 
